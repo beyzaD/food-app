a6511db495903da9a446725bf040659c5726dfa2
Alexander Pajer <alexander.pajer@integrations.at>
2021-02-13 19:53:11 +0100

    serverside model

--- FoodUI/src/environments/environment.ts
+++ FoodUI/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  authEnabled: false,
   apiurl: "https://localhost:5001/api/",
   appInsights: {
     instrumentationKey: "a11919be-f72c-4abb-8096-2402b14ac2f2",
