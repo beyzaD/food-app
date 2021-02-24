97125f17a480fa6454f5765b3871d2e7d73b1ce9
Alexander Pajer <alexander.pajer@integrations.at>
2021-02-24 12:23:48 +0100

    linux-removed

--- az-cli/create-api.sh
+++ az-cli/create-api.sh
@@ -7,6 +7,6 @@ loc=westeurope
 az group create -n $grp -l $loc
 
 # If FREE is not possiple take F1
-az appservice plan create -n $appPlan -g $grp --sku FREE --is-linux
+az appservice plan create -n $appPlan -g $grp --sku FREE 
 
 az webapp create -n $app -g $grp --plan $appPlan --runtime "DOTNET|5.0"
