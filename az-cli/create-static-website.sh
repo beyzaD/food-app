d89e80671e07c0c2357646182ceba18bebc53195
Alexander Pajer <alexander.pajer@integrations.at>
2021-04-06 08:12:03 +0200

    script update

---
+++ az-cli/create-static-website.sh
@@ -0,0 +1,20 @@
+rnd=020
+grp=foodapp
+loc=westeurope
+acct=ngfoodui$rnd
+path='./FoodUI/dist/FoodUI'
+
+az group create -n $grp -l $loc
+
+az storage account create -l $loc -n $acct -g $grp --sku Standard_LRS
+
+key=$(az storage account keys list -n $acct --query [0].[value] -o tsv)
+
+echo "Website Key: " $key
+
+ep=$(az storage account show -g $grp -n $acct --query "primaryEndpoints.web")
+
+echo "Primary Endpoint for Static Web: " $ep
+
+az storage blob service-properties update --account-name $acct --static-website --404-document error.html --index-document index.html
+
