8cb81a4df5bf92b9c830422c3ae1e63af6bd2c1b
Alexander Pajer <alexander.pajer@integrations.at>
2021-04-07 14:22:57 +0200

    food wp

--- FoodListSPFx/src/webparts/foodListWp/components/FoodListWp.tsx
+++ FoodListSPFx/src/webparts/foodListWp/components/FoodListWp.tsx
@@ -10,26 +10,27 @@ export default class FoodListWp extends React.Component<IFoodListWpProps, {}> {
         <div className={styles.container}>
           <div className={styles.row}>
             <div className={styles.column}>
-              <span className={styles.title}>
-                Welcome to Food List for Food App!
-              </span>
-              <div>
-                <img src="https://avatars3.githubusercontent.com/u/16348023?s=460&v=4" />{" "}
-                <p className={styles.subTitle}>
-                  A .NET Core Api and Angular UI implemented a Cloud Native App
-                  with step by step Installation Scripts used for ngDev, ngAdv,
-                  AZ-400, AZ-204 and MS-600
-                </p>
-                <p>A SharePoint SPFx WebPart</p>
+              <div className={styles.flexcol}>
+                <div className={styles.title}>
+                  Welcome to Food List for Food App!
+                </div>
+                <div className={styles.flexrow}>
+                  <img src="https://avatars3.githubusercontent.com/u/16348023?s=460&v=4" />{" "}
+                  <div className={styles.subTitle}>
+                    A .NET Core Api and Angular UI implemented a Cloud Native
+                    App with step by step Installation Scripts used for ngDev,
+                    ngAdv, AZ-400, AZ-204 and MS-600
+                  </div>
+                </div>
+                <a
+                  href="https://github.com/ARambazamba/FoodApp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.button}
+                >
+                  <span className={styles.label}>Learn more</span>
+                </a>
               </div>
-              <a
-                href="https://github.com/ARambazamba/FoodApp"
-                target="_blank"
-                rel="noopener noreferrer"
-                className={styles.button}
-              >
-                <span className={styles.label}>Learn more</span>
-              </a>
             </div>
           </div>
         </div>
