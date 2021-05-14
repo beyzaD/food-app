b04c72b0e5a8bf6a5842f6397e384ead96696f1c
Alexander Pajer <alexander.pajer@integrations.at>
2021-05-14 09:23:16 +0200

    config string typed

--- FoodApi/Controllers/MailController.cs
+++ FoodApi/Controllers/MailController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.Extensions.Configuration;
 
 namespace FoodApi
 {
@@ -13,16 +14,17 @@ namespace FoodApi
     public class MailController : ControllerBase
     {
         FoodConfig config { get; set; }
-        public MailController(IOptions<FoodConfig> cfg)
+        public MailController(IConfiguration cfg)
         {
-            config = (FoodConfig)cfg.Value;
+            var val = cfg.GetValue<string>("App:AuthEnabled");
+            config = cfg.Get<FoodConfig>();
         }
 
         [HttpPost]
         [Route("sendMail")]
         public ActionResult SendMail(FoodItem item)
         {
-            FoodApp.GraphHelper.Send("Save this for later",JsonSerializer.Serialize(item) , new[] { config.GraphCfg.mailSender }, config);
+            FoodApp.GraphHelper.Send("Take a look at this great food",JsonSerializer.Serialize(item) , new[] { config.GraphCfg.mailSender }, config);
             return Ok();
         }
     }
