e1d62da31c873968acc26ab01e1a5908e7011c7b
Alexander Pajer <alexander.pajer@integrations.at>
2021-05-16 19:48:46 +0200

    blob key removed

--- az-cli/create-blob-cdn.sh
+++ az-cli/create-blob-cdn.sh
@@ -2,6 +2,7 @@ rnd=030
 grp=foodapp-$rnd
 loc=westeurope
 acct=ngfoodui$rnd
+vault=foodvault-$rnd
 path='./FoodUI/dist/FoodUI'
 
 az group create -n $grp -l $loc
@@ -10,11 +11,13 @@ az storage account create -l $loc -n $acct -g $grp --sku Standard_LRS
 
 key=$(az storage account keys list -n $acct --query [0].[value] -o tsv)
 
-echo "Website Key: " $key
+az keyvault secret set --vault-name $vault --name "uiBlobKey" --value $key
+
+#  echo "Website Key: " $key
 
 ep=$(az storage account show -g $grp -n $acct --query "primaryEndpoints.web")
 
-echo "Primary Endpoint for Static Web: " $ep
+# echo "Primary Endpoint for Static Web: " $ep
 
 az storage blob service-properties update --account-name $acct --static-website --404-document error.html --index-document index.html
 
