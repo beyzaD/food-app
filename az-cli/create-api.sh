51a99a28d979006aa1c88baa210f947ccdab3af2
Alexander Pajer <alexander.pajer@integrations.at>
2021-05-19 08:51:04 +0200

    config

--- az-cli/create-api.sh
+++ az-cli/create-api.sh
@@ -3,6 +3,7 @@ grp=foodapp-$rnd
 loc=westeurope
 appPlan=foodplan-$rnd
 app=foodapi-$rnd
+cfg=foodconfig-$rnd
 
 az group create -n $grp -l $loc
 
@@ -10,4 +11,4 @@ az appservice plan create -n $appPlan -g $grp --sku FREE
 
 az webapp create -n $app -g $grp --plan $appPlan --runtime "DOTNET|5.0"
 
-echo "FoodApi Url: http://$app.azurewebsites.net"
+az appconfig kv set -n foodconfig-$rnd --key "apiUrl" --value http://$app.azurewebsites.net -y
