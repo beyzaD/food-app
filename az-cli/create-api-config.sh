16e2ca35d7596ce7d201b1d798c020368cb0e8af
Alexander Pajer <alexander.pajer@integrations.at>
2021-08-10 04:36:51 +0200

    config

---
+++ az-cli/create-api-config.sh
@@ -0,0 +1,12 @@
+rnd=030
+grp=foodapp-$rnd
+loc=westeurope
+appPlan=foodplan-$rnd
+app=foodapi-$rnd
+cfg=foodconfig-$rnd
+
+az group create -n $grp -l $loc
+
+az appservice plan create -n $appPlan -g $grp --sku FREE 
+
+az webapp create -n $app -g $grp --plan $appPlan --runtime "DOTNET|5.0"
