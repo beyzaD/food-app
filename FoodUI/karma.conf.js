b20afa017e63079f64a71f125069b8b2e542406b
Alexander Pajer <alexander.pajer@integrations.at>
2021-09-24 08:01:20 +0200

    del

--- FoodUI/karma.conf.js
+++ FoodUI/karma.conf.js
@@ -1,8 +1,7 @@
-module.exports = function (config) {
-  const puppeteer = require('puppeteer');
-  process.env.CHROME_BIN = puppeteer.executablePath();
-  console.log(process.env.CHROME_BIN)
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
+module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -11,26 +10,23 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-junit-reporter')
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
+      dir: require('path').join(__dirname, './coverage/FoodUI'),
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml', 'junit'],
-    junitReporter: {
-      outputDir: '../junit'
-    },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
-    singleRun: false
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true
   });
 };
