7ad143b060e7be1dab7237027855b05d861db017
Alexander Pajer <alexander.pajer@integrations.at>
2021-10-07 07:42:35 +0200

    auth

--- FoodUI/src/environments/environment.ts
+++ FoodUI/src/environments/environment.ts
@@ -1,16 +1,17 @@
 export const environment = {
   production: false,
-  authEnabled: false,
+  authEnabled: true,
   apiurl: "https://localhost:5001/",
   appInsights: {
     instrumentationKey: "da2aaf3f-cfcd-4741-a0f0-b6b15a0ff47c",
   },
   firebaseConfig: {
-    apiKey: "AIzaSyDK-xtrfT4YBzq4Hzmf5kEwHgHbYw4UQTc",
-    authDomain: "ngrx-firebase-fb51b.firebaseapp.com",
-    projectId: "ngrx-firebase-fb51b",
-    storageBucket: "ngrx-firebase-fb51b.appspot.com",
-    messagingSenderId: "141384369616",
-    appId: "1:141384369616:web:659d331b4ad7a3f16365d5",
+    apiKey: "AIzaSyCilhj9vCW1efrxg_lL674e2ywaFNFy_r8",
+    authDomain: "food-app-e377e.firebaseapp.com",
+    projectId: "food-app-e377e",
+    storageBucket: "food-app-e377e.appspot.com",
+    messagingSenderId: "922322176164",
+    appId: "1:922322176164:web:299216ca4ab080fcb9be31",
+    measurementId: "G-4QSQNC5L9W",
   },
 };
