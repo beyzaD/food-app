b5be49e81423281021be2db35ed7f4e7f20b6fd8
Alexander Pajer <alexander.pajer@integrations.at>
2022-01-08 14:35:52 +0100

    config to env

--- food-ui/src/environments/environment.prod.ts
+++ food-ui/src/environments/environment.prod.ts
@@ -5,10 +5,17 @@ export const environment = {
   azure: {
     applicationInsights: 'a196d36f-1782-4da4-8f95-a80585361df7',
     appReg: {
-      clientId: 'fb9e23e2-7727-40a3-9515-7f53d90c6cab',
+      clientId: 'd23642f7-9ccf-4165-92e7-919f625a5acc',
       authority:
         'https://login.microsoftonline.com/d92b247e-90e0-4469-a129-6a32866c0d0a/',
       redirectUri: 'http://localhost:4200/',
+      scopes: [
+        ['https://graph.microsoft.com/v1.0/me', ['user.read']],
+        [
+          'https://localhost:5001/food',
+          ['api://b509d389-361a-447b-afb2-97cc8131dad6/access_as_user'],
+        ],
+      ],
     },
   },
 };
