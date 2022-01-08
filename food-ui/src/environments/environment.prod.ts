bcd7b44dcf2c33b10283261f4fcbe0128c44f991
Alexander Pajer <alexander.pajer@integrations.at>
2022-01-08 13:21:31 +0100

    config service removed

--- food-ui/src/environments/environment.prod.ts
+++ food-ui/src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  authEnabled: false,
+  authEnabled: true,
+  apiUrl: 'https://localhost:5001/',
+  azure: {
+    applicationInsights: 'a196d36f-1782-4da4-8f95-a80585361df7',
+    appReg: {
+      clientId: 'fb9e23e2-7727-40a3-9515-7f53d90c6cab',
+      authority:
+        'https://login.microsoftonline.com/d92b247e-90e0-4469-a129-6a32866c0d0a/',
+      redirectUri: 'http://localhost:4200/',
+    },
+  },
 };
